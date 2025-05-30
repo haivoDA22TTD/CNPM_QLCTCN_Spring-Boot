501ba38264e8a897c5a116f3150adc8ced0bbe9f
Võ Chí Hải <vochihai7@gmail.com>
2025-05-30 17:23:15 +0700

    Dùng file cấu hình test

--- src/quan_ly_chi_tieu_ca_nhan/src/test/java/spring/boot/quan_ly_chi_tieu_ca_nhan/QuanLyChiTieuCaNhanApplicationTests.java
+++ src/quan_ly_chi_tieu_ca_nhan/src/test/java/spring/boot/quan_ly_chi_tieu_ca_nhan/QuanLyChiTieuCaNhanApplicationTests.java
@@ -2,13 +2,14 @@ package spring.boot.quan_ly_chi_tieu_ca_nhan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ActiveProfiles; 
+
 @ActiveProfiles("test") // Dùng file cấu hình test
 @SpringBootTest
 class QuanLyChiTieuCaNhanApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
